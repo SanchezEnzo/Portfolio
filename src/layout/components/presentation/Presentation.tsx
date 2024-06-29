48ce75405ee00420299239777a3bf097047d43fb
Enzo Sanchez <enzojsanchez98@gmail.com>
2024-06-29 09:01:34 -0300

    update presentation v1.0.1

--- src/layout/components/presentation/Presentation.tsx
+++ src/layout/components/presentation/Presentation.tsx
@@ -1,18 +1,20 @@
 export function Presentation() {
   return (
-    <section className='h-full w-full flex justify-center'>
+    <body className='h-full w-full flex justify-center'>
       <div className='max-w-[1280px] flex items-center gap-20'>
-        <div className='text-[--title-dark-mode] font-semibold flex flex-col gap-1'>
+        <div className='text-[--text-dark-mode] font-semibold flex flex-col gap-1'>
           <h2 className='text-4xl'>Hola, soy</h2>
-          <h1 className='text-5xl font-extrabold'>Enzo Sanchez</h1>
-          <span className='text-2xl'>Desarrollador Front-end</span>
+          <h1 className='text-5xl text-[--title-dark-mode] font-extrabold '>
+            Enzo Sanchez
+          </h1>
+          <span className='text-2xl pt-2'>Desarrollador Front-end</span>
         </div>
         <img
           className='rounded-full outline outline-[--outline-dark-mode] border border-[rgba(255,255,255,0.1)]'
-          src='./img/ProfilePhotoDarkMode.png'
-          alt='Foto Enzo Sanchez'
+          src='./img/ProfilePhotoDarkMode2.png'
+          alt='Foto de Enzo Sanchez'
         />
       </div>
-    </section>
+    </body>
   )
 }
--- src/layout/pages/Layout.tsx
+++ src/layout/pages/Layout.tsx
@@ -4,7 +4,7 @@ import { Proyects } from '../components/proyects/Proyects'
 
 export function Layout() {
   return (
-    <div className='bg-[--bg-dark-mode] min-h-screen flex flex-col gap-40'>
+    <div className='bg-[--bg-dark-mode] min-h-screen flex flex-col gap-20'>
       <div className='animate-progress-grow timeline-scroll fixed bottom-0 w-[0%] bg-[--small-text-dark-mode] h-1'></div>
       <Header />
       <Presentation />
