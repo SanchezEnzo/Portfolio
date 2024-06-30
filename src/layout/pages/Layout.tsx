38e285bca8d6ec91157a3ce4bc7d25026c57a55b
Enzo Sanchez <enzojsanchez98@gmail.com>
2024-06-30 11:24:21 -0300

    add dark config

--- src/layout/pages/Layout.tsx
+++ src/layout/pages/Layout.tsx
@@ -8,18 +8,18 @@ export function Layout() {
   const { darkMode } = useThemeMode()
 
   return (
-    <div
-      className={`min-h-screen flex flex-col gap-20 ${darkMode ? 'dark' : ''}`}
-    >
-      <div className='animate-progress-grow timeline-scroll fixed bottom-0 w-[0%] bg-[--small-text-dark-mode] h-1'></div>
-      <Header />
-      <SideBar />
-      <Presentation />
-      <Proyects />
-      <div className='fixed bottom-1 right-40'>
-        <span className='text-xl text-black font-semibold'>
-          ModeDark: {darkMode.toString()}
-        </span>
+    <div className={`${darkMode ? 'dark' : ''}`}>
+      <div className='min-h-screen flex flex-col gap-20 bg-[--bg-light-mode] dark:bg-[--bg-dark-mode]'>
+        <div className=' animate-progress-grow timeline-scroll fixed bottom-0 w-[0%] bg-[--text-light-mode]  h-1'></div>
+        <Header />
+        <SideBar />
+        <Presentation />
+        <Proyects />
+        <div className='fixed bottom-1 right-40'>
+          <span className='text-xl text-black font-semibold'>
+            ModeDark: {darkMode.toString()}
+          </span>
+        </div>
       </div>
     </div>
   )
