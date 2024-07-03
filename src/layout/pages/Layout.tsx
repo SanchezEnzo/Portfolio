b28d5f2e9f0dd2e8e1bc7ca435232ecc1d7e5ad6
Enzo Sanchez <enzojsanchez98@gmail.com>
2024-07-03 20:33:04 -0300

    fix navbar animation

--- src/layout/pages/Layout.tsx
+++ src/layout/pages/Layout.tsx
@@ -1,5 +1,7 @@
+// Layout.jsx
+import { Contact } from '../components/contact/Contact'
 import { Experience } from '../components/experience/Experience'
-import { Header } from '../components/header/Header'
+import { Header } from '../components/navbar/Navbar'
 import { Presentation } from '../components/presentation/Presentation'
 import { Projects } from '../components/projects/Projects'
 import { SideBar } from '../components/sidebar/SideBard'
@@ -9,14 +11,15 @@ export function Layout() {
   const { darkMode } = useThemeMode()
 
   return (
-    <div className={`${darkMode ? 'dark' : ''}, pb-40`}>
+    <div className={`${darkMode ? 'dark' : ''}`}>
       <div className='min-h-screen flex flex-col gap-40 bg-[--bg-light-mode] dark:bg-[--bg-dark-mode] items-center'>
-        <div className=' animate-progress-grow timeline-scroll fixed bottom-0 left-0 w-[0%] bg-[--text-light-mode] dark:bg-[--text-dark-mode] h-1 z-50'></div>
+        <div className='animate-progress-grow timeline-scroll fixed bottom-0 left-0 w-[0%] bg-[--text-light-mode] dark:bg-[--text-dark-mode] h-1 z-50'></div>
         <Header />
         <SideBar />
         <Presentation />
         <Projects />
         <Experience />
+        <Contact />
       </div>
     </div>
   )
