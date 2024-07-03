cfdd12ab1ffca30a9582f72347473353b3e4bd42
Enzo Sanchez <enzojsanchez98@gmail.com>
2024-07-03 20:27:58 -0300

    fix animation bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'selector',
   // ...
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {
       keyframes: {
@@ -10,20 +10,7 @@ export default {
           from: { width: '0%' },
           to: { width: '100%' }
         },
-        'enhance-header-light': {
-          to: {
-            background: 'rgba(250, 250, 250,.4)',
-            backdropFilter: 'blur(5px)',
-            paddingTop: '1px'
-          }
-        },
-        'enhance-header-dark': {
-          to: {
-            background: 'rgba(22, 22, 22,.4)',
-            backdropFilter: 'blur(5px)',
-            paddingTop: '1px'
-          }
-        },
+
         'spin-once': {
           from: { transform: 'rotate(0deg)' },
           to: { transform: 'rotate(90deg)' }
@@ -33,6 +20,7 @@ export default {
         'progress-grow': 'progress-grow auto linear',
         'enhance-header-light': 'enhance-header-light linear both',
         'enhance-header-dark': 'enhance-header-dark linear both',
+        'expand-header': 'expand-header linear both',
         'spin-once': 'spin-once 0.3s linear both'
       },
       transition: {}
